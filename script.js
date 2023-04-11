8783d903ea35594ec7283431c0d15039dcfb1960
MUKHTAR KHAN <106866680+MUKHTAR-02@users.noreply.github.com>
2023-04-12 00:03:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var web3;
+async function Connect(){
+  await window.web3.currentProvider.enable();
+  web3 = new Web3(window.web3.currentProvider);
+}
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("navbar").style.fontSize = "30px";
+  } else {
+    document.getElementById("navbar").style.fontSize = "90px";
+  }
+}
+
+function SubscribeButton() {
+  // window.alert("Welcome to our website");
+  
+// testing
+
+  var result = window.confirm('Are you sure?');
+    if (result === true) {
+      window.alert('Subscribed succesfully.');
+    } else { 
+      window.alert('You seem uncertain.');
+    }
+}
+
